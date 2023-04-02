aed7fa8545689e3c1f3356af1716a9bd472b05dc
micaalvarezm <malvarezmolina@udesa.edu.ar>
2023-04-02 20:55:28 -0300

    modulo de datos

---
+++ demo/data/modulo.js
@@ -0,0 +1,120 @@
+const data = {
+    usuario: [
+        {
+            nombre: "paula",
+            nombreDeUsuario: "paula123",
+            mail: "paula.rodriguez@gmail.com",
+            FotoDePerfil: "../imagenes/descarga.jpg",
+            id:"0",
+        }],
+
+    productos: [
+        {
+            id: "1",
+            nombre:"Conjunto 3 Tiras Aeroready Essentials Corte Clásico",
+            marca:"adidas",
+            modelo:"Gk9950 A",
+            descripcion:"Sin importar cómo lo usés, la tecnología AEROREADY se encarga de mantener tu cuerpo seco y cómodo de la mañana a la noche.",
+        },{
+            id: "2",
+            nombre:"Remera De Entrenamiento Train Essentials  ",
+            marca:"adidas",
+            modelo:"Ic7430",
+            descripcion:"La tecnología AEROREADY se encarga del sudor para que sientas comodidad en todo momento. Su diseño te ofrece una total libertad de movimiento, así que animate y levantá esas pesas.",
+        },{
+            id: "3",
+            nombre:"Remera Run It ",
+            marca:"adidas",
+            modelo:"Hb7473",
+            descripcion:"El tejido ligero y transpirable te brinda soporte desde el primer al último paso y la tecnología AEROREADY absorbe el sudor para mantener tu piel fresca y seca en todo momento.",
+        },{
+            id: "4",
+            nombre:"Buzo Con Capucha Essentials Logo Lineal Felpa",
+            marca:"adidas",
+            modelo:"Ic9367",
+            descripcion:"Este buzo con capucha adidas recompensa tu esfuerzo y el dolor muscular para que puedas rendir al máximo. Porque este buzo hace más fácil tu vida, y eso es lo que te merecés.",
+        },{
+            id: "5",
+            nombre:"Gorra Béisbol 3 Tiras Sarga (unisex)",
+            marca:"adidas",
+            modelo:"Hn1037",
+            descripcion:"Esta nueva versión del look clásico ofrece un tacto suave y un diseño con protección UV. Luce un logo adidas Badge of Sport que le da un nuevo giro a tu orgullo por las 3 Tiras.",
+        },{
+            id: "6",
+            nombre:"Remera Essentials Mezcla",
+            marca:"adidas",
+            modelo:"Hk2902",
+            descripcion:"Esta remera adidas es más que capaz de brindarte comodidad de la mañana a la noche. Está hecha de un tejido de una mezcla de poliéster y algodón suave y texturizado, pero a la vez muy fresco. ",
+        },{
+            id: "7",
+            nombre:"Calzas Farm Rio Feel Brilliant Aeroready Pretina Alta ",
+            marca:"adidas",
+            modelo:"Gs6337",
+            descripcion:"En el gimnasio, en la ciudad o en la carretera, movete siempre con estilo. Estas calzas adidas son el resultado de una colaboración con FARM Rio. Lucen llamativos estampados en la pierna y en la cintura de pretina alta.",
+        },{
+            id: "8",
+            nombre:"Shorts Essentials Chelsea 3 Tiras Aeroready",
+            marca:"adidas",
+            modelo:"Ic1485",
+            descripcion:"Diseñados para los días ajetreados, estos shorts adidas incorporan un calzoncillo interior de malla transpirable que ofrece mayor cobertura y comodidad en todo momento. El tejido AEROREADY con tecnología de absorción se encarga de mantener tu piel seca mientras te ejercitás.",
+        },{
+            id: "9",
+            nombre:"Zapatillas Runfalcon 3",
+            marca:"adidas",
+            modelo:"Hp7550",
+            descripcion:"Estas versátiles zapatillas de running adidas son perfectas para correr en el parque y después tomar un café con tus amigos. Su mediasuela Cloudfoam te ofrece una pisada cómoda desde el momento en que te las ponés.",
+        },{
+            id: "10",
+            nombre:"Rompevientos Tejido Essentials 3 Tiras",
+            marca:"adidas",
+            modelo:"Ib0378",
+            descripcion:"Esta campera adidas con las 3 Tiras es una capa exterior versátil que se sincroniza con todo lo que tenés en tu armario. Su diseño liviano con forro interno de malla permite la circulación del aire mientras la capucha te protege de la lluvia.",
+        },
+    ],
+
+    comentarios :[
+        {
+            id: "1",
+            usuario:"jualian.lopez",
+            comentario:"exelente preoducto",
+        },{
+            id: "2",
+            usuario:"hector12",
+            comentario:"muy buena calidad",
+        },{
+            id: "3",
+            usuario:"juancarlos_1",
+            comentario:"tal cual la descripcion de la venta",
+        },{
+            id: "4",
+            usuario:"lara_gonzales",
+            comentario:"me esperaba mas",
+        },{
+            id: "5",
+            usuario:"jorge.cabj",
+            comentario:"10 de 10 en todo sentido",
+        },{
+            id: "6",
+            usuario:"agustinaalvarez",
+            comentario:"que genialidad de producto",
+        },{
+            id: "7",
+            usuario:"luana.barrios",
+            comentario:"tal cual como lo esperaba",
+        },{
+            id: "8",
+            usuario:"antonelaa_russo1",
+            comentario:"muy buen producto, lastima que tardaron mucho con el envio",
+        },{
+            id: "9",
+            usuario:"username123",
+            comentario:"nada fuera de lo normal",
+        },{
+            id: "10",
+            usuario:"flortorres_23",
+            comentario:"le doy 3 estrellas, no es gran cosa",
+        },
+    ],
+}
+
+module.exports = data;
