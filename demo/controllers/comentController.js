3f0cac0d59db49891eba81aefee1ddcc234f8f59
TimoHerrera <therrera@udesa.edu.ar>
2023-04-05 12:56:53 -0300

    cambio en views

--- demo/controllers/comentController.js
+++ demo/controllers/comentController.js
@@ -1,7 +1,7 @@
 const data = require("../db/modulo");
 const comentcontroller= {
     index: function(req,res){
-        return res.render(`comentarios`,{comentarios: data.comentarios  })  // esto esta mal?? me aparece problema en la data
+        return res.render(`comentarios`,{comentarios: data.comentarios  })  
     }
 }
 
