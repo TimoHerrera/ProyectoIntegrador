217d82ba13884c26169471a88bd405aa657ed8f9
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2023-04-08 16:55:09 -0300

    agregar imgs faltantes a db

--- demo/db/modulo.js
+++ demo/db/modulo.js
@@ -58,6 +58,7 @@ const data = {
             marca:"adidas",
             modelo:"Hn1037",
             descripcion:"Esta nueva versión del look clásico ofrece un tacto suave y un diseño con protección UV. Luce un logo adidas Badge of Sport que le da un nuevo giro a tu orgullo por las 3 Tiras.",
+            imagen: "../imagenes/gorraBeisbolSarga.webp",
         },{
             id: "6",
             imagen:"",
@@ -65,6 +66,7 @@ const data = {
             marca:"adidas",
             modelo:"Hk2902",
             descripcion:"Esta remera adidas es más que capaz de brindarte comodidad de la mañana a la noche. Está hecha de un tejido de una mezcla de poliéster y algodón suave y texturizado, pero a la vez muy fresco. ",
+            imagen: "../imagenes/remeraEssentialsMezcla.webp",
         },{
             id: "7",
             imagen:"",
@@ -72,6 +74,7 @@ const data = {
             marca:"adidas",
             modelo:"Gs6337",
             descripcion:"En el gimnasio, en la ciudad o en la carretera, movete siempre con estilo. Estas calzas adidas son el resultado de una colaboración con FARM Rio. Lucen llamativos estampados en la pierna y en la cintura de pretina alta.",
+            imagen: "../imagenes/buzoCapuchaEssentials.webp",
         },{
             id: "8",
             imagen:"",
@@ -79,6 +82,7 @@ const data = {
             marca:"adidas",
             modelo:"Ic1485",
             descripcion:"Diseñados para los días ajetreados, estos shorts adidas incorporan un calzoncillo interior de malla transpirable que ofrece mayor cobertura y comodidad en todo momento. El tejido AEROREADY con tecnología de absorción se encarga de mantener tu piel seca mientras te ejercitás.",
+            imagen: "../imagenes/shortsEssentialsChelsea.webp",
         },{
             id: "9",
             imagen:"",
@@ -86,6 +90,7 @@ const data = {
             marca:"adidas",
             modelo:"Hp7550",
             descripcion:"Estas versátiles zapatillas de running adidas son perfectas para correr en el parque y después tomar un café con tus amigos. Su mediasuela Cloudfoam te ofrece una pisada cómoda desde el momento en que te las ponés.",
+            imagen: "../imagenes/zapatillasRunfalcon3.webp",
         },{
             id: "10",
             imagen:"",
@@ -93,6 +98,7 @@ const data = {
             marca:"adidas",
             modelo:"Ib0378",
             descripcion:"Esta campera adidas con las 3 Tiras es una capa exterior versátil que se sincroniza con todo lo que tenés en tu armario. Su diseño liviano con forro interno de malla permite la circulación del aire mientras la capucha te protege de la lluvia.",
+            imagen: "../imagenes/rompevientosEssentials.webp",
         },
     ],
 
