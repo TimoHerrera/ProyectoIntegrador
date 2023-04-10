5679941630c237b117a0b368fea33b08939a6187
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2023-04-10 11:42:59 -0300

    cambiooos

--- demo/controllers/sResultsController.js
+++
@@ -1,8 +0,0 @@
-const data = require("../db/modulo");
-const resultscontroller= {
-    index: function(req,res){
-        return res.render(`sResults.ejs`)
-    }
-}
-
-module.exports = resultscontroller
