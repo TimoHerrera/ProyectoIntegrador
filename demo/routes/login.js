4fd18736f28014393f32fc88051e40d56f13f54c
TimoHerrera <therrera@udesa.edu.ar>
2023-04-10 10:12:54 -0300

    saque refister y login que estaban de mas

--- demo/routes/login.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const logincontroller=require(`../controllers/loginController`)
-
-router.get (`/`,logincontroller.index);
-
-module.exports=router;
