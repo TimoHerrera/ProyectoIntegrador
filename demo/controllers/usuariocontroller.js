c9eb52d973e500614fcfcf02c577642bdcf52d22
TimoHerrera <therrera@udesa.edu.ar>
2023-04-10 11:04:26 -0300

    usuario y otros

--- demo/controllers/usuariocontroller.js
+++ demo/controllers/usuariocontroller.js
@@ -1,8 +1,8 @@
 const data = require("../db/modulo");
 const usuariocontroller= {
     index: function(req,res){
-        res.render(`productos`,{productos:data.productos})
-        return res.render(`usuario`,{usuario:data.usuario  })  
+         
+         return res.render(`usuario`, {usuario:data.usuario, productos: data.productos })  
     }
 }
 
