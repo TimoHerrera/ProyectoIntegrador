14e69177c4498140af0b0377cd0c72acb00ac97e
TimoHerrera <therrera@udesa.edu.ar>
2023-04-10 11:25:30 -0300

    usuarios/login y header

--- demo/controllers/usuariocontroller.js
+++ demo/controllers/usuariocontroller.js
@@ -2,8 +2,12 @@ const data = require("../db/modulo");
 const usuariocontroller= {
     index: function(req,res){
          
-         return res.render(`usuario`, {usuario:data.usuario, productos: data.productos })  
-    }
+         return res.render(`usuario`, {usuario:data.usuario, productos: data.productos })          
+    },
+    login: function(req, res){
+        return res.render("login")
+    },
 }
+
 
 module.exports = usuariocontroller
--- demo/routes/usuario.js
+++ demo/routes/usuario.js
@@ -3,5 +3,5 @@ var router = express.Router();
 const usuariocontroller=require(`../controllers/usuarioController`)
 
 router.get (`/`,usuariocontroller.index);
-
+router.get (`/login`,usuariocontroller.login);
 module.exports=router;
