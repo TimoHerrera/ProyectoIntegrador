c15818feda48204d663f742732fa5579c6fef19f
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2023-04-10 00:55:38 -0300

    correcciones y css

--- demo/routes/sResults.js
+++ demo/routes/sResults.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const sResultsController = require("../controllers/sResultsController")
 
-router.get (`/`,sResultsController.index);
+router.get ('/',sResultsController.index);
 
 module.exports=router;
