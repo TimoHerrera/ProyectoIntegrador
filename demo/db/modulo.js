ef8d8aa7404e84af714224e70d232c4b6a33024b
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2023-04-12 10:52:53 -0300

    prodDetail arreglado, falta imprimir en ejs la description

--- demo/db/modulo.js
+++ demo/db/modulo.js
@@ -22,12 +22,12 @@ const data = {
             marca:"adidas",
             modelo:"Gk9950 A",
             descripcion:"Sin importar cómo lo usés, la tecnología AEROREADY se encarga de mantener tu cuerpo seco y cómodo de la mañana a la noche.",
-            imagen: "../imagenes/conjunto3tiras.webp",
+            imagen: "/imagenes/conjunto3tiras.webp",
             comentarios:{
                 id: "1",
                 usuario:"jualian.lopez",
                 comentario:"exelente preoducto",
-                comentimg:"../imagenes/coment1.jpg",
+                comentimg:"/imagenes/coment1.jpg",
             }
         
         },{ 
@@ -36,12 +36,12 @@ const data = {
             marca:"adidas",
             modelo:"Ic7430",
             descripcion:"La tecnología AEROREADY se encarga del sudor para que sientas comodidad en todo momento. Su diseño te ofrece una total libertad de movimiento, así que animate y levantá esas pesas.",
-            imagen: "../imagenes/remeraEntrenamientoEssentials.webp",
+            imagen: "/imagenes/remeraEntrenamientoEssentials.webp",
             comentarios:{
                 id: "2",
                 usuario:"hector12",
                 comentario:"muy buena calidad",
-                comentimg:"../imagenes/coment2.jpg",
+                comentimg:"/imagenes/coment2.jpg",
             }
         },{
             id: "3",
@@ -50,12 +50,12 @@ const data = {
             marca:"adidas",
             modelo:"Hb7473",
             descripcion:"El tejido ligero y transpirable te brinda soporte desde el primer al último paso y la tecnología AEROREADY absorbe el sudor para mantener tu piel fresca y seca en todo momento.",
-            imagen: "../imagenes/remeraRunIt.webp",
+            imagen: "/imagenes/remeraRunIt.webp",
             comentarios:{
                 id: "3",
                 usuario:"juancarlos_1",
                 comentario:"tal cual la descripcion de la venta",
-                comentimg:"../imagenes/coment3.jpg",
+                comentimg:"/imagenes/coment3.jpg",
             }
         },{
             id: "4",
@@ -64,12 +64,12 @@ const data = {
             marca:"adidas",
             modelo:"Ic9367",
             descripcion:"Este buzo con capucha adidas recompensa tu esfuerzo y el dolor muscular para que puedas rendir al máximo. Porque este buzo hace más fácil tu vida, y eso es lo que te merecés.",
-            imagen: "../imagenes/buzoCapuchaEssentials.webp",
+            imagen: "/imagenes/buzoCapuchaEssentials.webp",
             comentarios:{
                 id: "4",
                 usuario:"lara_gonzales",
                 comentario:"me esperaba mas",
-                comentimg:"../imagenes/coment4.jpg",
+                comentimg:"/imagenes/coment4.jpg",
             }
         },{
             id: "5",
@@ -78,12 +78,12 @@ const data = {
             marca:"adidas",
             modelo:"Hn1037",
             descripcion:"Esta nueva versión del look clásico ofrece un tacto suave y un diseño con protección UV. Luce un logo adidas Badge of Sport que le da un nuevo giro a tu orgullo por las 3 Tiras.",
-            imagen: "../imagenes/gorraBeisbolSarga.webp",
+            imagen: "/imagenes/gorraBeisbolSarga.webp",
             comentarios: {
                 id: "5",
                 usuario:"jorge.cabj",
                 comentario:"10 de 10 en todo sentido",
-                comentimg:"../imagenes/coment5.jpg",
+                comentimg:"/imagenes/coment5.jpg",
             }
         },{
             id: "6",
@@ -92,12 +92,12 @@ const data = {
             marca:"adidas",
             modelo:"Hk2902",
             descripcion:"Esta remera adidas es más que capaz de brindarte comodidad de la mañana a la noche. Está hecha de un tejido de una mezcla de poliéster y algodón suave y texturizado, pero a la vez muy fresco. ",
-            imagen: "../imagenes/remeraEssentialsMezcla.webp",
+            imagen: "/imagenes/remeraEssentialsMezcla.webp",
             comentarios:{
                 id: "6",
                 usuario:"agustinaalvarez",
                 comentario:"que genialidad de producto",
-                comentimg:"../imagenes/coment6.jpg",
+                comentimg:"/imagenes/coment6.jpg",
             }
         },{
             id: "7",
@@ -106,12 +106,12 @@ const data = {
             marca:"adidas",
             modelo:"Gs6337",
             descripcion:"En el gimnasio, en la ciudad o en la carretera, movete siempre con estilo. Estas calzas adidas son el resultado de una colaboración con FARM Rio. Lucen llamativos estampados en la pierna y en la cintura de pretina alta.",
-            imagen: "../imagenes/buzoCapuchaEssentials.webp",
+            imagen: "/imagenes/buzoCapuchaEssentials.webp",
             comentarios:{
                 id: "7",
                 usuario:"luana.barrios",
                 comentario:"tal cual como lo esperaba",
-                comentimg:"../imagenes/coment7.jpg",
+                comentimg:"/imagenes/coment7.jpg",
             }
         },{
             id: "8",
@@ -120,12 +120,12 @@ const data = {
             marca:"adidas",
             modelo:"Ic1485",
             descripcion:"Diseñados para los días ajetreados, estos shorts adidas incorporan un calzoncillo interior de malla transpirable que ofrece mayor cobertura y comodidad en todo momento. El tejido AEROREADY con tecnología de absorción se encarga de mantener tu piel seca mientras te ejercitás.",
-            imagen: "../imagenes/shortsEssentialsChelsea.webp",
+            imagen: "/imagenes/shortsEssentialsChelsea.webp",
             comentarios:{
                 id: "8",
                 usuario:"antonelaa_russo1",
                 comentario:"muy buen producto, lastima que tardaron mucho con el envio",
-                comentimg:"../imagenes/coment8.jpg",
+                comentimg:"/imagenes/coment8.jpg",
             }
         },{
             id: "9",
@@ -134,12 +134,12 @@ const data = {
             marca:"adidas",
             modelo:"Hp7550",
             descripcion:"Estas versátiles zapatillas de running adidas son perfectas para correr en el parque y después tomar un café con tus amigos. Su mediasuela Cloudfoam te ofrece una pisada cómoda desde el momento en que te las ponés.",
-            imagen: "../imagenes/zapatillasRunfalcon3.webp",
+            imagen: "/imagenes/zapatillasRunfalcon3.webp",
             comentarios:{
                 id: "9",
                 usuario:"username123",
                 comentario:"nada fuera de lo normal",
-                comentimg:"../imagenes/coment9.jpg",
+                comentimg:"/imagenes/coment9.jpg",
             }
         },{
             id: "10",
@@ -148,12 +148,12 @@ const data = {
             marca:"adidas",
             modelo:"Ib0378",
             descripcion:"Esta campera adidas con las 3 Tiras es una capa exterior versátil que se sincroniza con todo lo que tenés en tu armario. Su diseño liviano con forro interno de malla permite la circulación del aire mientras la capucha te protege de la lluvia.",
-            imagen: "../imagenes/rompevientosEssentials.webp",
+            imagen: "/imagenes/rompevientosEssentials.webp",
             comentarios: {
                 id: "10",
                 usuario:"flortorres_23",
                 comentario:"le doy 3 estrellas, no es gran cosa",
-                comentimg:"../imagenes/coment10.jpg",
+                comentimg:"/imagenes/coment10.jpg",
             
             },},
     ],
