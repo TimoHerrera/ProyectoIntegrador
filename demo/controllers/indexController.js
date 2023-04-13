d838da7b9eee2d132fb9eefdeae6f984921ad804
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2023-04-12 23:15:31 -0300

    sresults en controller

--- demo/controllers/indexController.js
+++ demo/controllers/indexController.js
@@ -3,7 +3,9 @@ const indexController = {
     index: function(req,res){
         return res.render("index",{productos:data.productos, usuario:data.usuario})
     },
+    result: function(req,res){
+        return res.render("sResults",{productos:data.productos, usuario:data.usuario})
 }
-
+}
 
 module.exports = indexController
