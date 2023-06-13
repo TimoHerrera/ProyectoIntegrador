2f5e0be5daa5a9c7ddc38323609f7902e26f1847
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2023-06-13 17:38:08 -0300

    mini

--- demo/database/config/config.js
+++ demo/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root", //preguntar que onda que yo tengo mac
+    "password": null, //preguntar que onda que yo tengo mac
     "database": "proyectointegrador",
     "host": "127.0.0.1",
     "dialect": "mysql"
